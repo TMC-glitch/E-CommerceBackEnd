e77910be6ff7a76fa53fe5ff62c1bc244503f111
TMChambers <Tchaiwanda.chambers@gmail.com>
2021-08-31 13:07:07 -0400

    E-commerce commit

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  Tag.findAll(
+    {
+      include: {
+        model: Product,
+        attributes: ["Product_tag"],
+      },
+    }).then(data => res.json(data))
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ["product_tag"],
+    }
+  }).then(data => res.json(data))
+  // be sure to include its associated Product data
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+  Tag.create(req.body)
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
